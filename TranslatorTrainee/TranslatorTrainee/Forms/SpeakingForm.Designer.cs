024f2eb5f6d50c94f45d1cec2e6a099cd147120b
Emil <emilnizamov9@gmail.com>
2022-04-08 16:38:05 +0300

    Speaking Form

---
+++ TranslatorTrainee/TranslatorTrainee/Forms/SpeakingForm.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace TranslatorTrainee.Forms;
+
+partial class SpeakingForm
+{
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem("Barack Obama (2:24)");
+            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem("Vladimir Putin (5:23)");
+            this.listView = new System.Windows.Forms.ListView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.trackName = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView
+            // 
+            this.listView.Alignment = System.Windows.Forms.ListViewAlignment.Default;
+            this.listView.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.listView.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1,
+            listViewItem2});
+            this.listView.Location = new System.Drawing.Point(0, 27);
+            this.listView.Margin = new System.Windows.Forms.Padding(0);
+            this.listView.MultiSelect = false;
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(385, 646);
+            this.listView.TabIndex = 0;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.List;
+            this.listView.SelectedIndexChanged += new System.EventHandler(this.listView_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(385, 27);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.trackName);
+            this.panel1.Location = new System.Drawing.Point(396, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(867, 670);
+            this.panel1.TabIndex = 2;
+            // 
+            // trackName
+            // 
+            this.trackName.AutoSize = true;
+            this.trackName.Location = new System.Drawing.Point(27, 108);
+            this.trackName.Name = "trackName";
+            this.trackName.Size = new System.Drawing.Size(50, 20);
+            this.trackName.TabIndex = 0;
+            this.trackName.Text = "label1";
+            // 
+            // SpeakingForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1262, 673);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listView);
+            this.MaximumSize = new System.Drawing.Size(1280, 720);
+            this.MinimumSize = new System.Drawing.Size(1280, 720);
+            this.Name = "SpeakingForm";
+            this.Text = "SpeakingForm";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private ListView listView;
+    private TextBox textBox1;
+    private Panel panel1;
+    private Label trackName;
+}
---
+++ TranslatorTrainee/TranslatorTrainee/Forms/SpeakingForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TranslatorTrainee.Forms;
+public partial class SpeakingForm : Form
+{
+    public SpeakingForm()
+    {
+        InitializeComponent();
+        listView.View = View.Tile;
+        listView.LabelEdit = false;
+        listView.GridLines = true;
+        listView.Items[1]
+    }
+
+    private void listView_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    private void textBox1_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+}
