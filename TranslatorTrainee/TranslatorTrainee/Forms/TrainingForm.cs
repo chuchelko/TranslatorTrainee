c71395ba5a426d57bf05a597c1d2f92dafa2548f
ruzwn <ruzwn@users.noreply.github.com>
2022-04-08 17:18:41 +0300

    Added training form

---
+++ TranslatorTrainee/TranslatorTrainee/Forms/TrainingForm.cs
@@ -0,0 +1,14 @@
+﻿namespace TranslatorTrainee.Forms;
+
+public partial class TrainingForm : Form
+{
+	public TrainingForm()
+	{
+		InitializeComponent();
+	}
+
+	public void startRecordingButton_Click(object sender, EventArgs e)
+	{
+		 
+	}
+}
